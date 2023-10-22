d6839719dfd1de2e8a3cfce543ced42ed21f4b2e
Artem Matrashov <artem.m2013@gmail.com>
2023-10-23 02:32:17 +0300

    add Lesson serializer

--- main/serializers.py
+++ main/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
-from main.models import Course
+from main.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
         fields = '__all__'
 
