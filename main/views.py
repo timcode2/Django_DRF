d99149fab8ce3db2a6bdfa552393bda871f2ecf9
Artem Matrashov <artem.m2013@gmail.com>
2023-10-23 02:33:12 +0300

    add generics for class Lesson

--- main/views.py
+++ main/views.py
@@ -1,10 +1,32 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from main.models import Course
-from main.serializers import CourseSerializer
+from main.models import Course, Lesson
+from main.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
