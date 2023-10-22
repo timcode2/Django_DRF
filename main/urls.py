2fa920d9ec32786ef8c282c8a55a0e1bed9e6ee9
Artem Matrashov <artem.m2013@gmail.com>
2023-10-23 02:32:57 +0300

    add generics urls for model Lesson

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from main.apps import MainConfig
 from rest_framework.routers import DefaultRouter
 
-from main.views import CourseViewSet
+from main.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MainConfig.name
 
@@ -10,5 +13,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete')
 ] + router.urls
