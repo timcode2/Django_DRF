cca07471ab6679eea3da0e04918b59a309150bbe
Artem Matrashov <artem.m2013@gmail.com>
2023-10-25 22:51:34 +0300

    add model Payment

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -22,10 +23,35 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='описание урока')
     video_link = models.URLField(verbose_name='ссылка на видео')
 
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курсы', **NULLABLE)
+
     def __str__(self):
         return f'{self.name}'
 
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
+
+
+class Payment(models.Model):
+
+    PAYMENT_CHOICES = [
+        ('Cash', 'Наличные'),
+        ('Transfer', 'Перевод')
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь',
+                             **NULLABLE)
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='оплаченный курс', **NULLABLE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='оплаченный урок', **NULLABLE)
+    payment_amount = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=150, choices=PAYMENT_CHOICES, verbose_name='Способо оплаты')
+
+    def __str__(self):
+        return f'{self.course if self.course else self.lesson} ({self.payment_date})'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
 
