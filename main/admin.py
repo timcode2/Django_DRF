5a735469b184d38204b45d4667fa53dde7c68369
Artem Matrashov <artem.m2013@gmail.com>
2023-10-25 22:51:47 +0300

    add admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Course, Lesson, Payment
+
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Payment)
