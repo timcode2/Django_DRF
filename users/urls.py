ae1bcb7e7bbcd9f8cfaba31edd6072f77c296e99
Artem Matrashov <artem.m2013@gmail.com>
2023-10-25 22:50:59 +0300

    add user

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from rest_framework.routers import DefaultRouter
+
+from users.views import UserViewSet
+
+app_name = UsersConfig.name
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+
+] + router.urls
