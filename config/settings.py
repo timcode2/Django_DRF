72c8843ca9a5f35e769d796e784ae9767d0f1bdb
Artem Matrashov <artem.m2013@gmail.com>
2023-12-09 02:11:15 +0300

    add docker

--- config/settings.py
+++ config/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangodrf',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'qwerty'
+        'PASSWORD': 'qwerty',
+        'HOST': 'db'
     }
 }
 
